8f6aa487823bfd3f577373a165bc949e51af9279
amine <amine@aminePC>
2018-09-15 12:07:51 +0200

    Create Spring Boot Project

---
+++ spring-boot-angular-example/src/main/java/com/mydevgeek/springbootangularexample/SpringBootAngularExampleApplication.java
@@ -0,0 +1,12 @@
+package com.mydevgeek.springbootangularexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAngularExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAngularExampleApplication.class, args);
+	}
+}

f0845ca641180585ced822faed4a1d34afcf92df
amine <amine@aminePC>
2018-10-09 19:25:55 +0200

    Web service Working !! :)

--- spring-boot-angular-example/src/main/java/com/mydevgeek/SpringBootAngularExampleApplication.java
+++ spring-boot-angular-example/src/main/java/com/mydevgeek/SpringBootAngularExampleApplication.java
@@ -1,4 +1,4 @@
-package com.mydevgeek.springbootangularexample;
+package com.mydevgeek;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

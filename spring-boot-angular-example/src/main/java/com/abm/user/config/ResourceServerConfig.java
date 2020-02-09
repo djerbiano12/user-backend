e9a7a39396b0c7c4099683678bac7ab7a6b63b1a
amine <amine@amine>
2020-02-09 13:33:05 +0100

    Aout du filter et adaptation de spring security oauth2 pour angular

--- spring-boot-angular-example/src/main/java/com/abm/user/config/ResourceServerConfig.java
+++ spring-boot-angular-example/src/main/java/com/abm/user/config/ResourceServerConfig.java
@@ -4,15 +4,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
 
 @Configuration
 @EnableResourceServer
-public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+	private static final String RESOURCE_ID = "resource_id";
+
+	@Override
+	public void configure(ResourceServerSecurityConfigurer resources) {
+		resources.resourceId(RESOURCE_ID).stateless(false);
+	}
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/users/auth").permitAll()
-								.antMatchers("/users/**").authenticated();
+        http.
+                anonymous().disable()
+                .authorizeRequests()
+                .antMatchers("/users/**").access("hasRole('ADMIN')")
+                .and().exceptionHandling().accessDeniedHandler(new OAuth2AccessDeniedHandler());
 	}
-	
+
 }

81d63fc443478108f256db734c18bf549d9efffc
amine <amine@amine>
2020-03-28 11:47:31 +0100

    Configuration Maven

---
+++ spring-boot-angular-example/src/test/java/com/mydevgeek/springbootangularexample/TestApplication.java
@@ -0,0 +1,8 @@
+package com.mydevgeek.springbootangularexample;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+}

d84c46bf7f160f54598fa17f089dae2c6a85abc9
amine <amine@amine>
2020-04-29 15:08:40 +0200

    ticket: https://github.com/djerbiano12/user-frontend/issues/9

--- spring-boot-angular-example/src/main/java/com/abm/user/controller/UserController.java
+++ spring-boot-angular-example/src/main/java/com/abm/user/controller/UserController.java
@@ -1,20 +1,28 @@
 package com.abm.user.controller;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.abm.user.dto.UserDto;
 import com.abm.user.dto.UserDtoMapper;
+import com.abm.user.repository.entity.Role;
 import com.abm.user.repository.entity.User;
 import com.abm.user.service.RoleService;
 import com.abm.user.service.UserService;
-
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -58,8 +66,10 @@ public class UserController {
 	@Secured({ROLE_ADMIN})
 	@RequestMapping(method = RequestMethod.POST)
 	public UserDto saveUser(@RequestBody UserDto user) throws ParseException {
+		Set<Role> roles = new HashSet<Role>();	
 		user.setPassword(encoder.encode(user.getPassword()));
-		user.getRoles().add(roleService.getRoleByName(user.getRole()));
+		roles.add(roleService.getRoleByName(user.getRole()));
+		user.setRoles(roles);
 		return UserDtoMapper.convertToDto(userService.create(UserDtoMapper.convertToEntity(user, modelMapper)), modelMapper);
 	}
 
